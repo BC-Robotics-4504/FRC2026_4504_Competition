5c6745a99c4ace150f83aa4b7c99d94db7dc36fe
KnockoffofKnockoff <156373475+KnockoffofKnockoff@users.noreply.github.com>
2026-02-21 15:43:54 -0500

    commented out contradictory generated code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -77,10 +77,10 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+        // joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        // joystick.b().whileTrue(drivetrain.applyRequest(() ->
+        //     point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        // ));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
@@ -94,8 +94,8 @@ public class RobotContainer {
 
         drivetrain.registerTelemetry(logger::telemeterize);
 
-        joystick.b().whileTrue(new Intake(fuelSubsystem));
         joystick.a().whileTrue(new LaunchSequence(fuelSubsystem));
+        joystick.b().whileTrue(new Intake(fuelSubsystem));
     }
 
     public Command getAutonomousCommand() {
