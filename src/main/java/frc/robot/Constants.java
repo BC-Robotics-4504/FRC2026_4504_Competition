a0c4af4a9c995b398dce30cbeb0763205e38f4ce
Gerald Bowers <coldfire70@gmail.com>
2026-04-04 11:54:15 -0400

    initial photonvision

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean constants. This class should not be used for any other
@@ -105,5 +111,15 @@ public final class Constants {
     // help avoid turning too fast and beign difficult to control
     public static final double DRIVE_SCALING = .7;
     public static final double ROTATION_SCALING = .8;
+  }
+
+  public static final class PhotonConstants {
+    //Load the AprilTag field layout from the default field definition
+    public static final AprilTagFieldLayout TAG_LAYOUT =
+      AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+    
+    public static final Transform3d ROBOT_TO_CAM =
+      new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+    
   }
 }
---
+++ src/main/java/frc/robot/subsystems/PhotonSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+import static frc.robot.Constants.PhotonConstants.*;
+
+public class PhotonSubsystem extends SubsystemBase {
+    public PhotonSubsystem() {
+        
+    }
+}
